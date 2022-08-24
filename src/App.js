023585c77b74d10bbb16f76b43f2ce944d785d9d
Maxmiliano F Braga <maxmiliano@gmail.com>
2022-08-24 09:13:22 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+            </p>
+        <p>
+Hi!!! I'm here!
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
